8b5dc19d0a81347e5a9cdbc6f21f6149e1bb7bd4
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-06-13 13:28:51 +0300

    solved issue with closing paranthesis in calculate_washing_price method

--- app/main.py
+++ app/main.py
@@ -28,7 +28,8 @@ class CarWashStation:
             (car.comfort_class
              * (self.clean_power - car.clean_mark)
              * self.average_rating
-             / self.distance_from_city_center), 1)
+             / self.distance_from_city_center), 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
